b34a4dadc0297cb729e9deba8a2d8ecc929e880d
Alan Smith <arsmithjr@gmail.com>
2025-06-17 02:21:09 -0400

    Updates.

--- app/data/400_error.py
+++ app/data/400_error.py
@@ -2,5 +2,5 @@ def get_400_error_content(url_for):
     return {
         "image_src": url_for('static', filename='images/errors/400.png'),
         "image_alt": "400 Bad Request",
-        "message": "400 Bad Request\nThe request could not be understood by the server due to malformed syntax or invalid input."
+        "message": "Nothing to see here"
     }
