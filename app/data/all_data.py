cab4b98e4b034d26093d990ab7f482e85da53fdf
Alan Smith <arsmithjr@gmail.com>
2025-07-30 17:15:17 -0400

    Will work on GitHub Actions again. Removed for now.

--- app/data/all_data.py
+++ app/data/all_data.py
@@ -116,8 +116,7 @@ def get_home_card():
         'image_src': 'images/content/cartoonized-alan-smith.png',
         'image_alt': 'Cartoonized Alan Smith',
         'card_title': 'Welcome to My Portfolio',
-        'card_text': ("Hi, I'm Alan Smith—a technology enthusiast, lifelong learner, and passionate problem solver. "
-                     "This website is a showcase of my journey, projects, and achievements, designed to give you a glimpse into my professional world and personal growth. "
-                     "Explore my work, discover my story, and check out the certifications page—I'm hoping to have more than just three badges there soon!"
+        'card_text': ("Hi, I'm Alan Smith, a technology enthusiast, lifelong learner, and passionate problem solver. "
+                     "This website showcases my journey and achievements, giving you a glimpse into my professional world and personal growth. "
                      "<i class='fas fa-face-wink' style='color:#f7b731; font-size:1.3em; vertical-align:middle;'></i>")
     }
