ea34d901cd463e57f2fe7e4bc242279fb974c1ea
Alan Smith <arsmithjr@gmail.com>
2025-07-30 16:56:57 -0400

    Cleaned up code again and error pages fixed.

--- app/data/404_error.py
+++ app/data/404_error.py
@@ -1,6 +1,6 @@
-def get_400_error_content(url_for):
+def get_404_error_content(url_for):
     return {
-        "image_src": url_for('static', filename='images/errors/400.png'),
-        "image_alt": "400 Bad Request",
-        "message": "Nothing to see here"
+        "image_src": url_for('static', filename='images/errors/404.png'),
+        "image_alt": "404 Page Not Found",
+        "message": "Oops! The page you're looking for doesn't exist. Let's get you back on track."
     }
